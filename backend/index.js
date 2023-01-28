1e5b4bcbee9fffd7e7c7b451a49f25a73e31e9d7
Anna Phung <aphung1@umd.edu>
2023-01-27 23:57:58 -0500

    Changed the backend server port to 80. Created the front end skeleton

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
     res.json("hello world");
