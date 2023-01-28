8c2f79d2e9117eff4c27cbfb071c2267ccf8a89d
bzhang <scentedsoap>
2023-01-28 14:01:25 -0500

    pls work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Form from './pages/pages/Form'
 
 function App() {
   return (
